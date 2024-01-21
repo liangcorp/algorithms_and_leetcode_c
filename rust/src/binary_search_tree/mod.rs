c369fa054bb6c2dca72f3d1a236068645a6c110c
Chen Liang <chen.liang@ie.ibm.com>
2024-01-21 18:32:19 +0000

    binary search tree implementation

--- rust/src/binary_search_tree/mod.rs
+++ rust/src/binary_search_tree/mod.rs
@@ -1,2 +1,61 @@
-pub mod search;
+use std::cmp::Ordering;
 
+pub struct Node {
+    data: i32,
+    left: Pointer,
+    right: Pointer,
+}
+
+type Pointer = Option<Box<Node>>;
+
+impl Node {
+    #[allow(dead_code)]
+    pub fn new(data: i32) -> Node {
+        Node {
+            data,
+            left: None,
+            right: None,
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn insert(&mut self, data: i32) {
+        let new_node;
+
+        match data.cmp(&self.data) {
+            Ordering::Less => {
+                if self.left.is_some() {
+                    self.left.as_mut().unwrap().insert(data);
+                } else {
+                    new_node = Box::new(Node::new(data));
+                    self.left = Some(new_node);
+                }
+            }
+            Ordering::Greater => {
+                if self.right.is_some() {
+                    self.right.as_mut().unwrap().insert(data);
+                } else {
+                    new_node = Box::new(Node::new(data));
+                    self.right = Some(new_node);
+                }
+            }
+            Ordering::Equal => (),
+        }
+    }
+
+    pub fn inorder(&self) {
+        if self.left.is_some() && self.right.is_some() {
+            self.left.as_ref().unwrap().inorder();
+            println!("{}", self.data);
+            self.right.as_ref().unwrap().inorder();
+        } else if self.left.is_some() && self.right.is_none() {
+            self.left.as_ref().unwrap().inorder();
+            println!("{}", self.data);
+        } else if self.left.is_none() && self.right.is_some() {
+            println!("{}", self.data);
+            self.right.as_ref().unwrap().inorder();
+        } else {
+            println!("{}", self.data);
+        }
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,16 +5,30 @@
  * Date: 11-Feb-2023
  */
 
+
 mod insert_sort;
+mod binary_search_tree;
+// use computer_algorithms::binary_search_tree;
 
 fn main() {
-    let mut array = [30, 10, 60, 70, 50, 30, 20, 80, 40, 90];
+    // let mut array = [30, 10, 60, 70, 50, 30, 20, 80, 40, 90];
 
-    println!("{:?}", &array);
+    // println!("{:?}", &array);
     // insert_sort::forward::mono_increasing(&mut array);
     // insert_sort::forward::mono_decreasing(&mut array);
     // insert_sort::backward::mono_increasing(&mut array);
-    insert_sort::backward::mono_decreasing(&mut array);
+    // insert_sort::backward::mono_decreasing(&mut array);
 
-    println!("{:?}", &array);
+    // println!("{:?}", &array);
+    //
+    let mut tree_node = binary_search_tree::Node::new(8);
+    tree_node.insert(3);
+    tree_node.insert(1);
+    tree_node.insert(6);
+    tree_node.insert(10);
+    tree_node.insert(14);
+    tree_node.insert(13);
+    tree_node.insert(4);
+    tree_node.insert(7);
+    tree_node.inorder();
 }
